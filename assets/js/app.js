19819eb0b1a7c3d7c3811376501446112f1614c3
mickequeen <mickequeen@gmail.com>
2018-02-18 14:40:29 -0300

    commit inicial, rescatando preguntas

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+const btnStart = document.getElementById('btnStart');
+
+btnStart.addEventListener('click', function(event) {
+  let evento = event.target;
+  getTrivia();
+})
+
+function getTrivia() {
+  fetch(`https://opentdb.com/api.php?amount=10`)
+    .then(function(response) {
+    // Turns the the JSON into a JS object
+      return response.json();
+    })
+    .then(function(categories) {
+      var firstQuestion = categories.results[0].question;
+      $('#container').append(firstQuestion);
+    });
+}
